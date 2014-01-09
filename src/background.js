7e57dc9b9f1ccf00c675784ce8716a0d6114e67b
Gabriel Reitz Giannattasio <gartz@l6.com.br>
2014-01-08 23:12:58 -0200

    Moved files to src folder

--- src/background.js
+++ src/background.js
@@ -14,11 +14,11 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
   if (tab.url.contains('facebook.com')) {
     chrome.browserAction.getBadgeText({}, function (result) {
-      if (result === 'on') {
-        chrome.browserAction.setBadgeText({text: ''});
-      } else {
-        chrome.browserAction.setBadgeText({text: 'on'});
-      }
+//       if (result === 'on') {
+//         chrome.browserAction.setBadgeText({text: ''});
+//       } else {
+//         chrome.browserAction.setBadgeText({text: 'on'});
+//       }
     });
     chrome.tabs.executeScript({
       file: 'youtube.js'
